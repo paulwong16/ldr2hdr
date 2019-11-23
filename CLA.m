b0494fbd4d6fa78abb4bf699817b4408e7508b12
Zhijie Wang <zhijie.wang@uwaterloo.ca>
2019-11-22 21:08:49 -0500

    leave at dwt

--- CLA.m
+++ CLA.m
@@ -50,9 +50,86 @@ C_t_k = jnd_contrast(img_sets);
 %% Well-exposedness measure
 
     function E_t_k = exposedness(img, img_set, thd0, thd1)
+        img = double(img);
         sigma_L=32;
         sigma_M=64;
         sigma_H=32;
+        [height,width] = size(img);
+        mu_l = 0;
+        mu_l_num = 0;
+        mu_h = 0;
+        mu_h_num = 0;
+        region_map = zeros(height, width);
+        for i = 1:height
+            for j = 1:width
+                if img(i,j) < thd0
+                    region_map(i,j) = 1;
+                    mu_l = mu_l + img(i,j);
+                    mu_l_num = mu_l_num + 1;
+                elseif img(i,j) > thd1
+                    region_map(i,j) = 3;
+                    mu_h = mu_h + img(i,j);
+                    mu_h_num = mu_h_num + 1;
+                else
+                    region_map(i,j) = 2;
+                end
+            end
+        end
+        
+        mu_l = mu_l / mu_l_num;
+        mu_h = mu_h / mu_h_num;
+        r_l = mu_l_num / (height*width);
+        target = img;
+        
+        for i = 1:height
+            for j = 1:width
+                switch region_map(i,j)
+                    case 1
+                        if mu_l > 64
+                            target(i,j) = 64;
+                        elseif mu_l >= 32 && mu_l <= 64
+                            target(i,j) = mu_l;
+                        elseif mu_l < 32 && r_l > 0.5
+                            target(i,j) = 64;
+                        elseif mu_l < 32 && r_l >= 0.25 && r_l <= 0.5
+                            target(i,j) = r_l * 128;
+                        elseif mu_l < 32 && r_l < 0.25
+                            target(i,j) = 32;
+                        end
+                    case 2
+                        target(i,j) = 128;
+                    case 3
+                        if mu_h > 224
+                            target(i,j) = mu_h;
+                        elseif mu_h >= 192 && mu_h <= 224
+                            target(i,j) = 224;
+                        else
+                            target(i,j) = 192;
+                        end
+                end      
+            end
+        end
+        
+        [height,width,img_set_size] = size(img_set);
+        E_t_k = zeros(height,width,img_set_size);
+        for k = 1:img_set_size
+            tmp = (img_set(:,:,k) - target) .^2;
+            tmp_l = exp(-(tmp / (2 * sigma_L^2)));
+            tmp_m = exp(-(tmp / (2 * sigma_M^2)));
+            tmp_h = exp(-(tmp / (2 * sigma_H^2)));
+            for i = 1:height
+                for j = 1:width
+                    switch region_map(i,j)
+                        case 1
+                            E_t_k(i,j,k) = tmp_l(i,j);
+                        case 2
+                            E_t_k(i,j,k) = tmp_m(i,j);
+                        case 3
+                            E_t_k(i,j,k) = tmp_h(i,j);
+                    end
+                end
+            end
+        end
         
     end
 
@@ -67,8 +144,26 @@ E_t_k = exposedness(gray, img_sets, thd0, thd1);
     end
 
 W_k = weight_generator(C_t_k, E_t_k);
-        
-res = C_t_k;
+
+imshow(uint8(E_t_k(:,:,1)));
+
+%% Fuse luminance map
+
+    function F = fuse_sets(img_set, weight_set)
+        L = 3;
+        [height, width, sets] = size(img_set);
+        coeff = zeros(height/2, width/2, sets, 4);
+        for k = 1:sets
+            [coeff(:,:,k,1), coeff(:,:,k,2), coeff(:,:,k,3), coeff(:,:,k,4)] = dwt2(img_set(:,:,k),'haar');
+        end
+        F_ = coeff .* impyramid(weight_set, 'reduce');
+        F_ = sum(F_, 3);
+        F = idwt2(F_(:,:,1), F_(:,:,2), F_(:,:,3), F_(:,:,4), 'haar');
+    end
+
+F = fuse_sets(img_sets, W_k);
+
+res = E_t_k(:,:,1);
 
 end
 
